e7a7bc3bb28c80aba1c23093e725dad6249719a0
takahumi0806 <yabuta0439@hotmail.com>
2021-02-21 22:42:14 +0900

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function () {
+  console.log('Node.js is listening to PORT:' + server.address().port);
+});
+
+app.set('view engine', 'ejs');
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res, next) {
+  res.render('index', {});
+});
